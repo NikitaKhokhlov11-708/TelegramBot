b63b1f6f71f218d728677b0bf7061f7ca8c0bd42
NikitaKhokhlov11-708 <nkhokhlov11708@gmail.com>
2018-12-18 19:52:10 +0300

    Product adding updated

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -78,18 +78,50 @@ namespace TelegramBot
                                 try
                                 {
                                     SqlDataReader myReader = null;
-                                    SqlCommand myCommand = new SqlCommand("select * from Users where id = " + last.Message.From.Id,
+                                    SqlCommand myCommand = new SqlCommand("select * from Fridge where id = " + last.Message.From.Id,
                                                                              conn);
                                     myReader = myCommand.ExecuteReader();
+
                                     if (!myReader.HasRows)
                                     {
                                         myReader.Close();
-                                        myCommand = new SqlCommand("insert into Users values (" + last.Message.From.Id + ")", conn);
+                                        myCommand = new SqlCommand("insert into Fridge values (" + last.Message.From.Id + ",'')", conn);
                                         myCommand.ExecuteNonQuery();
-                                        message = "Вы успешно зарегистрировались.";
+                                        message = "Вы успешно зарегистрировались. ";
                                     }
                                     else
-                                        message = "Ваш холодильник:";
+                                    {
+                                        message = "Ваш холодильник: \n";
+                                        myReader.Close();
+                                        SqlDataReader getReader = null;
+                                        myCommand = new SqlCommand("select * from Fridge where id = " + last.Message.From.Id,
+                                                                             conn);
+                                        var products = new List<string>();
+                                        getReader = myCommand.ExecuteReader();
+                                        Console.WriteLine(getReader.HasRows);
+                                        getReader.Read();
+                                        var temp = getReader.GetString(1);
+                                        Console.WriteLine(temp == null);
+                                        var productsArr = temp.Split(';');
+                                        if (productsArr != null && temp != "")
+                                        {
+                                            foreach (var product in productsArr)
+                                            {
+                                                var arr = product.Split(',');
+                                                if (arr.Length == 3)
+                                                    products.Add(arr[0] + ' ' + arr[1] + ' ' + arr[2]);
+                                                else
+                                                    continue;
+                                            }
+                                            foreach (var elem in products)
+                                            {
+                                                message += elem + "\n";
+                                            }
+                                        }
+                                        else
+                                            message += "Пусто\n";
+                                    }
+                                    message += "\nДобавить ингредиенты можно при помощи команды /fridge_add Название, кол-во, ед. измерения \nПример: \n/fridge_add Томаты, 9, шт";
                                 }
                                 catch (Exception e)
                                 {
@@ -102,6 +134,59 @@ namespace TelegramBot
                                 message = "Неизвестная команда.";
                                 break;
                         }
+
+                        if (last.Message.Text.Contains("/fridge_add "))
+                        {
+                            SqlConnection conn = new SqlConnection("server=localhost;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=TelegramBot;");
+                            conn.Open();
+                            SqlDataReader getReader = null;
+                            var myCommand = new SqlCommand("select * from Fridge where id = " + last.Message.From.Id,
+                                                                 conn);
+                            getReader = myCommand.ExecuteReader();
+                            var products = new List<string>();
+                            Console.WriteLine(getReader.HasRows);
+                            getReader.Read();
+                            var temp = getReader.GetString(1);
+                            var productsArr = temp.Split(';');
+                            Console.WriteLine(productsArr.Length);
+                            if (productsArr != null && productsArr[0] != "")
+                            {
+                                foreach (var product in productsArr)
+                                {
+                                    var arr = product.Split(',');
+                                    if (arr.Length == 3)
+                                    {
+                                        products.Add(arr[0] + ' ' + arr[1] + ' ' + arr[2]);
+                                        Console.WriteLine(product[product.Length - 1]);
+                                    }
+                                }
+                            }
+                            getReader.Close();
+
+                            var ingr = last.Message.Text.Substring(12);
+                            var parse = ingr.Split(',');
+                            string ins = "";
+                            if (parse != null && parse.Length == 3)
+                            {
+                                products.Add(parse[0].Trim() + ' ' + parse[1].Trim() + ' ' + parse[2].Trim());
+                                Console.WriteLine(products.Last());
+                                foreach (var prod in products)
+                                {
+                                    var insparse = prod.Split(' ');
+                                    ins += insparse[0] + "," + insparse[1] + "," + insparse[2] + ";";
+                                }
+                                var insCommand = new SqlCommand("update Fridge set products = '" + ins + "' where id = " + last.Message.From.Id, conn);
+                                insCommand.ExecuteNonQuery();
+                                message = "Продукт \"" + parse[0].Trim() + "\" успешно добавлен!";
+                            }
+                            else
+                                message = "Ошибка";
+
+                            getReader.Close();
+                        }
+
 
                         await Bot.SendTextMessageAsync(last.Message.From.Id, message);
                     }
