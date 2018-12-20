873ac647566306ec905e1fa635afa33f65a17328
AlexanderTarasov11-708 <aleksandrtarasov53@gmail.com>
2018-12-21 00:26:37 +0300

    --корзина продуктов

---
+++ TelegramBot/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    public class Basket
+    {
+        public List<Product> listProducts;
+    }
+
+
+}
